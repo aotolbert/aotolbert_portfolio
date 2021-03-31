513517d2987b3907ba5a1cf98751d2e434bdc654
Adam Tolbert <aotolbert@gmail.com>
2021-03-30 23:09:32 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,19 @@
+console.log('app.js is alive!');
+
+let toggleMenu = () => {
+    let menuContent = document.getElementsByClassName('menuContent')[0];
+    if(!menuContent) {
+        console.log('enuContent was falsey');
+    }
+    let isCurrentlyHidden = menuContent.classList.contains('hidden');
+
+    if(isCurrentlyHidden) {
+        console.log('The menu content is currently hidden, should reveal it');
+        menuContent.classList.remove('hidden');
+    } else {
+        console.log('The menu content is not hidden, should hide it');
+        menuContent.classList.add('hidden');
+    }
+
+
+}
